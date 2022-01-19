3f9b508b2b3b2740cad62eb7ccff7de9834e2511
li zi <li@entangled.one>
2022-01-19 15:37:16 -0800

    small font change

--- src/lib/components/Flippable.svelte
+++ src/lib/components/Flippable.svelte
@@ -1,107 +1,9 @@
 <script>
-export let frontName = 'front'
-export let backName = 'back'
+  export let frontName = 'front'
+  export let backName = 'back'
 
-let flipped = false
+  let flipped = false
 </script>
-
-<style>
-.flippable-wrap {
-  --focus-border: 1px dashed transparent;
-
-  border: var(--focus-border);
-  padding: var(--shim);
-
-  perspective: 200vw;
-  transition: transform 0.4s;
-}
-
-.flippable-wrap:hover {
-  /* transform: scale(1.05); */
-}
-
-.flippable {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  transform-style: preserve-3d;
-  transition-duration: 0.8s
-}
-
-.side {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-
-  border-radius: 0.4rem;
-  border: 1px dotted var(--pokemon-color-medium-light);
-}
-
-.side > :global([slot]) {
-  width: 100%;
-  height: 100%;
-}
-
-.side[aria-hidden=false] {
-  box-shadow: 2px 2px 0 var(--pokemon-color-medium-light);
-  transition: box-shadow 250ms;
-  transition-delay: 500ms;
-}
-
-.back {
-  transform: rotateY(180deg);
-}
-
-.flipped {
-  transform: rotateY(180deg);
-}
-
-/* https://webaim.org/techniques/css/invisiblecontent/ */
-/* hide button when card is not targeted */
-button {
-  border: unset;
-  background: unset;
-  width: 100%;
-  padding: var(--shim);
-  margin-top: var(--shim);
-
-  cursor: pointer;
-  opacity: 0;
-}
-
-/* show button as hint text when card is targeted */
-button:focus, .flippable-wrap:hover button {
-  opacity: 1;
-}
-
-button::-moz-focus-inner {
-  border: unset;
-}
-
-button::after {
-  content: '';
-  box-sizing: content-box;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  border: var(--focus-border);
-}
-
-button:focus::after {
-  border-color: black;
-}
-
-.flippable-wrap:focus-within {
-  border-color: black;
-}  
-
-.flippable-wrap:focus-within button:focus::after {
-  border-color: transparent;
-}  
-</style>
 
 <div 
   class="flippable-wrap expanded" 
@@ -130,3 +32,103 @@ button:focus::after {
     >Flip to {flipped ? frontName : backName}</button>
 
 </div>
+
+<style>
+  .flippable-wrap {
+    --focus-border: 1px dashed transparent;
+
+    border: var(--focus-border);
+    padding: var(--shim);
+
+    perspective: 200vw;
+    transition: transform 0.4s;
+  }
+
+  .flippable-wrap:hover {
+    /* transform: scale(1.05); */
+  }
+
+  .flippable {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+    transition-duration: 0.8s
+  }
+
+  .side {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+
+    border-radius: 0.4rem;
+    border: 1px dotted var(--pokemon-color-medium-light);
+  }
+
+  .side > :global([slot]) {
+    width: 100%;
+    height: 100%;
+  }
+
+  .side[aria-hidden=false] {
+    box-shadow: 2px 2px 0 var(--pokemon-color-medium-light);
+    transition: box-shadow 250ms;
+    transition-delay: 500ms;
+  }
+
+  .back {
+    transform: rotateY(180deg);
+  }
+
+  .flipped {
+    transform: rotateY(180deg);
+  }
+
+  /* https://webaim.org/techniques/css/invisiblecontent/ */
+  /* hide button when card is not targeted */
+  button {
+    border: unset;
+    background: unset;
+    font: inherit;
+    color: inherit;
+    width: 100%;
+    padding: var(--shim);
+    margin-top: var(--shim);
+
+    cursor: pointer;
+    opacity: 0;
+  }
+
+  /* show button as hint text when card is targeted */
+  button:focus, .flippable-wrap:hover button {
+    opacity: 1;
+  }
+
+  button::-moz-focus-inner {
+    border: unset;
+  }
+
+  button::after {
+    content: '';
+    box-sizing: content-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border: var(--focus-border);
+  }
+
+  button:focus::after {
+    border-color: black;
+  }
+
+  .flippable-wrap:focus-within {
+    border-color: black;
+  }  
+
+  .flippable-wrap:focus-within button:focus::after {
+    border-color: transparent;
+  }  
+</style>
