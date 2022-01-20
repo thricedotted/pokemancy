199016947f4e89b005c2c5d3ebd2c4e5800e728e
li zi <li@entangled.one>
2022-01-19 20:07:47 -0800

    opengraph metadata?

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,22 @@
 <script>
+	import { page } from '$app/stores'
+
 	import Pokemancy from '$lib/components/Pokemancy.svelte'
 	import About from '$lib/components/About.svelte'
 </script>
 
 <svelte:head>
 	<title>Pokémancy</title>
+
+	<meta name="description" content="Divination... with Pokémon." />
+	<meta name="robots" content="index,follow" />
+
+	<meta property="og:title" content="Pokémancy" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://pokemancy.entangled.one" />
+	<meta property="og:description" content="Divination... with Pokémon." />
+	<meta property="og:image" content="{$page.url.origin}/pokemancy.png" />
+	<meta name="twitter:card" content="summary" />
 </svelte:head>
 
 <Pokemancy />
