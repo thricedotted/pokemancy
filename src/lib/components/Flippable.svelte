4fca4b2641a8fa7dba7a66e68b9f3e525b59034b
li zi <li@entangled.one>
2022-01-19 19:17:24 -0800

    fix backface visibility in safari

--- src/lib/components/Flippable.svelte
+++ src/lib/components/Flippable.svelte
@@ -61,6 +61,7 @@
     width: 100%;
     height: 100%;
     backface-visibility: hidden;
+    -webkit-backface-visibility: hidden;
 
     border-radius: 0.4rem;
     border: 1px dotted var(--pokemon-color-medium-light);
